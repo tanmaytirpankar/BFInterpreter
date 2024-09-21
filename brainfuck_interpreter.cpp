a9e1e709905e3929b4e6b4a88ae4b1ae5235ee59
tanmaytirpankar <tirpankartanmay@gmail.com>
2024-09-20 18:48:55 -0600

    Corrected profiler

--- brainfuck_interpreter.cpp
+++ brainfuck_interpreter.cpp
@@ -7,17 +7,15 @@
 #include <cstdlib>
 #include <chrono>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
 bool enable_profiler = false;
 
+unordered_map<int, int> instruction_count;
 unordered_map<int, int> loop_map;
-unordered_map<int, int> inner_most_loop_map;
-unordered_map<int, int> simple_loops;
-unordered_map<char, int> instruction_count;
-unordered_map<int, int> loop_execution_count;
+unordered_map<string, int> non_simple_loops;
+unordered_map<string, int> simple_loops;
 
 // Function to open file and read content
 string read_file(const string &file_name) {
@@ -46,18 +44,8 @@ string filter_text(const string &text) {
 // Initialize global variables
 void initialize_globals() {
   loop_map.clear();
-  inner_most_loop_map.clear();
+  non_simple_loops.clear();
   simple_loops.clear();
-
-  // Initialize instruction counter
-  instruction_count['>'] = 0;
-  instruction_count['<'] = 0;
-  instruction_count['+'] = 0;
-  instruction_count['-'] = 0;
-  instruction_count['.'] = 0;
-  instruction_count[','] = 0;
-  instruction_count['['] = 0;
-  instruction_count[']'] = 0;
 }
 
 // Preprocessing to match loops and store their positions
@@ -85,7 +73,6 @@ bool preprocess_loops(const string &text) {
   }
 
   if(enable_profiler) {
-    assert(loop_stack.empty());
     bool is_inner_most_loop = false;
     bool is_simple_loop = false;
     int loop_body_pointer_offset = 0;
@@ -93,26 +80,21 @@ bool preprocess_loops(const string &text) {
 
     for (int i = 0; i < text.size(); i++) {
       if (text[i] == '[') {
-        loop_stack.push(i);
         is_inner_most_loop = true;
         is_simple_loop = true;
         loop_body_pointer_offset = 0;
+        start_pointer_change = 0;
       } else if (text[i] == ']') {
-        if (loop_stack.empty()) {
-          cerr << "Mismatched ']' at position " << i << endl;
-          exit(1);
-        }
-        int open_pos = loop_stack.top();
         if (is_inner_most_loop) {
-          inner_most_loop_map[open_pos] = i;
           is_inner_most_loop = false;
           if (is_simple_loop && loop_body_pointer_offset == 0 &&
               (start_pointer_change == -1 || start_pointer_change == 1)) {
-            simple_loops[open_pos] = i;
+            simple_loops[text.substr(loop_map[i], i - loop_map[i] + 1)] = 0;
             is_simple_loop = false;
+          } else {
+            non_simple_loops[text.substr(loop_map[i], i - loop_map[i] + 1)] = 0;
           }
         }
-        loop_stack.pop();
       } else if (is_inner_most_loop && is_simple_loop) {
         if (text[i] == '.' || text[i] == ',') {
           is_simple_loop = false;
@@ -138,22 +120,21 @@ void parse_program(const string &text) {
   int ptr = 0; // memory pointer
 
   // Start with a larger tape size to avoid frequent resizing
-  vector<int> tape(30000, 0);
+  vector<char> tape(30000, 0);
 
   // Process each character in the input program
   while (ip < text.size()) {
     char command = text[ip];
+    instruction_count[ip]++;
 
     switch (command) {
       case '>':  // move pointer right
         if(enable_profiler)
-          instruction_count['>']++;
         ptr++;
         break;
 
       case '<':  // move pointer left
         if(enable_profiler)
-          instruction_count['<']++;
         if (ptr > 0)
           ptr--;
         else {
@@ -164,43 +145,39 @@ void parse_program(const string &text) {
 
       case '+':  // increment the value at current cell
         if(enable_profiler)
-          instruction_count['+']++;
-        tape[ptr] = (tape[ptr] + 1) % 256;
+        tape[ptr]++;
         break;
 
       case '-':  // decrement the value at current cell
         if(enable_profiler)
-          instruction_count['-']++;
-        tape[ptr] = (tape[ptr] - 1 + 256) % 256;
+        tape[ptr]--;
         break;
 
       case '.':  // output the value at current cell as character
         if(enable_profiler)
-          instruction_count['.']++;
         putchar(tape[ptr]);
         break;
 
       case ',':  // read a character from input into the current cell
         if(enable_profiler)
-          instruction_count[',']++;
         tape[ptr] = getchar();
         break;
 
       case '[':  // begin loop
-        if(enable_profiler) {
-          instruction_count['[']++;
-          if(inner_most_loop_map.find(ip) != inner_most_loop_map.end() && ip < inner_most_loop_map[ip]) {
-            loop_execution_count[ip]++;
-          }
-        }
         if (tape[ptr] == 0) {
           ip = loop_map.at(ip);  // jump to the matching ']'
+        } else if(enable_profiler) {
+          if(non_simple_loops.find(text.substr(ip, loop_map[ip] - ip + 1)) != non_simple_loops.end()) {
+            non_simple_loops[text.substr(ip, loop_map[ip] - ip + 1)]++;
+          }
+          if(simple_loops.find(text.substr(ip, loop_map[ip] - ip + 1)) != simple_loops.end()) {
+            simple_loops[text.substr(ip, loop_map[ip] - ip + 1)]++;
+          }
         }
         break;
 
       case ']':  // end loop
         if(enable_profiler)
-          instruction_count[']']++;
         if (tape[ptr] != 0) {
           ip = loop_map.at(ip);  // jump back to the matching '['
         }
@@ -218,48 +195,38 @@ void parse_program(const string &text) {
     // Print instruction count
     cout << "Instruction counts:" << endl;
     for (auto &pair : instruction_count) {
+      cout << pair.first << "(" << text[pair.first] << "):" << pair.second << endl;
+    }
+    cout << endl;
+
+    // Print sizes of loop_map, non_simple_loops and simple_loops
+//    cout << "#Loops: " << loop_map.size() / 2  << endl;
+    cout << "#Simple loops: " << simple_loops.size() << endl;
+    cout << "#Non-simple loops: " << non_simple_loops.size() << endl;
+    cout << endl;
+
+    // sort the simple and non-simple loops by frequency
+    vector<pair<string, int>> simple_loops_vec(simple_loops.begin(), simple_loops.end());
+    vector<pair<string, int>> non_simple_loops_vec(non_simple_loops.begin(), non_simple_loops.end());
+    sort(simple_loops_vec.begin(), simple_loops_vec.end(), [](const pair<string, int> &a, const pair<string, int> &b) {
+      return a.second > b.second;
+    });
+    sort(non_simple_loops_vec.begin(), non_simple_loops_vec.end(), [](const pair<string, int> &a, const pair<string, int> &b) {
+      return a.second > b.second;
+    });
+
+    // Print the simple loop bodies
+    cout << "Simple loop bodies:" << endl;
+    for (auto &pair : simple_loops_vec) {
       cout << pair.first << ": " << pair.second << endl;
     }
     cout << endl;
 
-    // Print sizes of loop_map, inner_most_loop_map and simple_loops
-//    cout << "#Loops: " << loop_map.size() << endl;
-    cout << "#Inner most loops: " << inner_most_loop_map.size() << endl;
-    cout << "#Simple loops: " << simple_loops.size() << endl;
-    cout << endl;
-
-    // sort the loop execution counts by value
-    vector<pair<int, int>> sorted_loop_execution_count;
-    for (auto &pair : loop_execution_count) {
-      sorted_loop_execution_count.push_back(pair);
+    // Print the non-simple loop bodies
+    cout << "Non-simple loop bodies:" << endl;
+    for (auto &pair : non_simple_loops_vec) {
+      cout << pair.first << ": " << pair.second << endl;
     }
-    sort(sorted_loop_execution_count.begin(), sorted_loop_execution_count.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
-        return a.second > b.second;
-    });
-
-    // Print the inner most loop execution counts that are simple loops
-    cout << "Simple loop execution counts:" << endl;
-    for (auto &pair : sorted_loop_execution_count) {
-      if (simple_loops.find(pair.first) != simple_loops.end()) {
-        cout << pair.first << ": " << pair.second << endl;
-      }
-    }
-    cout << endl;
-
-    // Print the inner most loop execution counts that are not simple loops
-    cout << "Non-simple loop execution counts:" << endl;
-    for (auto &pair : sorted_loop_execution_count) {
-      if (simple_loops.find(pair.first) == simple_loops.end()) {
-        cout << pair.first << ": " << pair.second << endl;
-      }
-    }
-
-    // Print the simple loop bodies
-//    cout << "Simple loop bodies:" << endl;
-//    for (auto &pair : simple_loops) {
-//      cout << text.substr(pair.first + 1, pair.second - pair.first - 1) << endl;
-//    }
-
   }
 }
 
