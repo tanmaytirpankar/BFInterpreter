3d8d93d2f6ac302b2c9f3ec84f16549f73574816
tanmaytirpankar <tirpankartanmay@gmail.com>
2024-10-02 15:58:42 -0600

    Started implementing optimizations

--- brainfuck_compiler.cpp
+++ brainfuck_compiler.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <unordered_map>
 
 using namespace std;
+
+bool enable_profiler = false;
+
+unordered_map<int, int> loop_map;
+unordered_map<string, int> non_simple_loops;
+unordered_map<string, int > simple_loops;
+unordered_map<string, string> optimizations;
+
 
 // Function to open file and read content
 string read_file(const string &file_name) {
@@ -26,6 +35,87 @@ string filter_text(const string &text) {
     }
   }
   return filtered;
+}
+
+// Initialize global variables
+void initialize_globals() {
+  loop_map.clear();
+  non_simple_loops.clear();
+  simple_loops.clear();
+}
+
+// Preprocessing to match loops and store their positions
+bool preprocess_loops(const string &text) {
+  stack<int> loop_stack;
+
+  for (int i = 0; i < text.size(); i++) {
+    if (text[i] == '[') {
+      loop_stack.push(i);
+    } else if (text[i] == ']') {
+      if (loop_stack.empty()) {
+        cerr << "Mismatched ']' at position " << i << endl;
+        exit(1);
+      }
+      int open_pos = loop_stack.top();
+      loop_map[open_pos] = i;
+      loop_stack.pop();
+      loop_map[i] = open_pos;
+    }
+  }
+
+  if (!loop_stack.empty()) {
+    cerr << "Mismatched '[' at position " << loop_stack.top() << endl;
+    exit(1);
+  }
+
+  if (enable_profiler) {
+    bool is_inner_most_loop = false;
+    bool is_simple_loop = false;
+    int loop_body_pointer_offset = 0;
+    int start_pointer_change = 0;
+
+    for (int i = 0; i < text.size(); i++) {
+      if (text[i] == '[') {
+        is_inner_most_loop = true;
+        is_simple_loop = true;
+        loop_body_pointer_offset = 0;
+        start_pointer_change = 0;
+      } else if (text[i] == ']') {
+        if (is_inner_most_loop) {
+          is_inner_most_loop = false;
+          if (is_simple_loop && loop_body_pointer_offset == 0 &&
+              (start_pointer_change == -1 || start_pointer_change == 1)) {
+            simple_loops[text.substr(loop_map[i], i - loop_map[i] + 1)] = 0;
+            is_simple_loop = false;
+          } else {
+            non_simple_loops[text.substr(loop_map[i], i - loop_map[i] + 1)] = 0;
+          }
+        }
+      } else if (is_inner_most_loop && is_simple_loop) {
+        if (text[i] == '.' || text[i] == ',') {
+          is_simple_loop = false;
+        } else if ((text[i] == '+' || text[i] == '-') && loop_body_pointer_offset == 0) {
+          if (text[i] == '+') {
+            start_pointer_change++;
+          } else {
+            start_pointer_change--;
+          }
+        } else if (text[i] == '<' || text[i] == '>') {
+          loop_body_pointer_offset += (text[i] == '<') ? -1 : 1;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+void add_optimizations() {
+  // Add optimizations here
+  // Example:
+  optimizations["[-]"] = "   mov byte [rsi], 0";
+  optimizations["[+]"] = "   mov byte [rsi], 0";
+  optimizations["[-<<<<<<<<->>>>>>>>]"] = "   sub rsi, 8";
 }
 
 // Compiles brainfuck to x86_64 assembly
@@ -52,7 +142,10 @@ void compile_program(const string &text, string output_file) {
   int loop_counter = 0;
   int curr_loop=0;
 
-  for(char bf_op : text) {
+  int ip = 0;  // instruction pointer
+
+  while (ip < text.size()) {
+    char bf_op = text[ip];
     switch(bf_op) {
       case '>':
         // Increment the data pointer
@@ -95,6 +188,7 @@ void compile_program(const string &text, string output_file) {
         asm_file << "   syscall" << endl;
         break;
       case '[':
+
         // Start of a loop
         loop_stack.push(loop_counter);
         // Label the loop
@@ -104,6 +198,15 @@ void compile_program(const string &text, string output_file) {
         // Jump to the end of the loop if the byte is 0
         asm_file << "   je loop_end_" << loop_counter << endl;
         loop_counter++;
+
+        if(enable_profiler) {
+            if(non_simple_loops.find(text.substr(ip, loop_map[ip] - ip + 1)) != non_simple_loops.end()) {
+              non_simple_loops[text.substr(ip, loop_map[ip] - ip + 1)]++;
+            }
+            if(simple_loops.find(text.substr(ip, loop_map[ip] - ip + 1)) != simple_loops.end()) {
+              simple_loops[text.substr(ip, loop_map[ip] - ip + 1)]++;
+            }
+        }
         break;
       case ']':
         // End of a loop
@@ -124,6 +227,8 @@ void compile_program(const string &text, string output_file) {
         // Ignore any other character
         break;
     }
+
+    ip++; // Move to the next instruction
   }
 
   // Close the program
@@ -143,8 +248,38 @@ int main(int argc, char *argv[]) {
       return 1;
     }
 
+    // Check for flag "-p" to enable profiler
+    if (argc == 4 && string(argv[3]) == "-p") {
+      enable_profiler = true;
+    }
+
     string text = read_file(argv[1]);
     text = filter_text(text);
+    initialize_globals();
+    // Preprocess loop start and end positions
+    preprocess_loops(text);
+
+    // Add optimizations
+    add_optimizations();
+
+    if(enable_profiler) {
+      cout << "#Simple loops: " << simple_loops.size() << endl;
+      cout << "#Non-simple loops: " << non_simple_loops.size() << endl;
+      cout << endl;
+
+      // Print the simple loop bodies
+      cout << "Simple loop bodies:" << endl;
+      for (auto &pair: simple_loops) {
+        cout << pair.first << ": " << pair.second << endl;
+      }
+      cout << endl;
+
+      // Print the non-simple loop bodies
+      cout << "Non-simple loop bodies:" << endl;
+      for (auto &pair: non_simple_loops) {
+        cout << pair.first << ": " << pair.second << endl;
+      }
+    }
 
     compile_program(text, argv[2]);
 
--- scripts/compile_and_execute.py
+++ scripts/compile_and_execute.py
@@ -1,8 +1,14 @@
 import os
 import sys
+import time
 
 if __name__ == '__main__':
     path = sys.argv[1]
+
+    enable_profiler = False
+    if sys.argv[2] == "-p":
+        enable_profiler = True
+
     scratch_dir = os.getcwd() + "/../scratch/"
 
     # get filename from path (remove the extension)
@@ -11,6 +17,18 @@ if __name__ == '__main__':
     filename = os.path.splitext(os.path.basename(path))[0]
 
     # Call the bf_compiler on the filename
-    os.system("../build-debug/bin/brainfuck_compiler " + root + '/' + filename + ".b " + scratch_dir + filename + ".asm")
+    os.system("../build-debug/bin/brainfuck_compiler " +
+              root + '/' + filename + ".b " +
+              scratch_dir + filename + ".asm" +
+              " -p" if enable_profiler else "")
     os.system("nasm -f elf64 " + scratch_dir + filename + ".asm")
     os.system("ld " + scratch_dir + filename + ".o -o " + scratch_dir + filename)
+
+    print("\n")
+    # Measure execution time of next command
+    start = time.time()
+
+    os.system(scratch_dir + filename)
+
+    end = time.time()
+    print("Execution time: ", end-start, " seconds")
